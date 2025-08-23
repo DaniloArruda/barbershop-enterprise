c0269d8892e198be8eba8315c6e73c0bafa4d3bc
Danilo Arruda <darruda.silva2@gmail.com>
2025-08-23 15:31:13 -0300

    First commit

---
+++ src/main/kotlin/com/danilo/barbershop/enterprise/domain/Barber.kt
@@ -0,0 +1,8 @@
+package com.danilo.barbershop.enterprise.domain
+
+import java.util.UUID
+
+class Barber(
+    val id: UUID,
+    val name: String,
+)
---
+++ src/test/kotlin/com/danilo/barbershop/enterprise/BarbershopEnterpriseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.danilo.barbershop.enterprise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BarbershopEnterpriseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
